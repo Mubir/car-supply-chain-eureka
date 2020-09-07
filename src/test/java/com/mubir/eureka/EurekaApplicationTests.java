d5e9d398f22d4a6f87a5c99c6771f7c676a68216
Munir <info.mubir@gmail.com>
2020-09-07 10:02:28 +0900

    initial

---
+++ src/test/java/com/mubir/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mubir.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
